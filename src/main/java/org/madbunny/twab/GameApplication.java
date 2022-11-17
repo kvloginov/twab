4f812f6c9ee637242d063f57b5fb097ce085beb4
kvloginov <kostghost@gmail.com>
2022-11-17 23:53:34 +0600

    add project layout

---
+++ src/main/java/org/madbunny/twab/GameApplication.java
@@ -0,0 +1,20 @@
+package org.madbunny.twab;
+
+import org.madbunny.vsrat2d.api.*;
+
+public class GameApplication {
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final String WINDOW_TITLE = "Sample Game";
+
+    public static void main(String[] args) {
+        var game = new GameCore(WINDOW_WIDTH, WINDOW_HEIGHT);
+        new Application()
+                .setWindowWidth(WINDOW_WIDTH)
+                .setWindowHeight(WINDOW_HEIGHT)
+                .setWindowTitle(WINDOW_TITLE)
+                .setInitializer(game::initialize)
+                .run(game::update);
+        System.exit(0);
+    }
+}
---
+++ src/main/java/org/madbunny/twab/GameCore.java
@@ -0,0 +1,29 @@
+package org.madbunny.twab;
+
+import org.madbunny.vsrat2d.api.ApplicationContext;
+import org.madbunny.vsrat2d.api.Color;
+import org.madbunny.vsrat2d.api.FrameContext;
+import org.madbunny.vsrat2d.api.Point2D;
+
+public class GameCore {
+    private static final Color BACKGROUND_COLOR = new Color(0.2f, 0.2f, 0.2f);
+    private static final Color TEXT_COLOR = new Color(1, 0, 0);
+
+    private final int windowWidth;
+    private final int windowHeight;
+    private int mainFontId;
+
+    public GameCore(int windowWidth, int windowHeight) {
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+    }
+
+    public void initialize(ApplicationContext ctx) {
+        mainFontId = ctx.fonts().createFont("arial.ttf", 24);
+    }
+
+    public void update(FrameContext ctx) {
+        ctx.screen().clear(BACKGROUND_COLOR);
+        ctx.screen().drawText(mainFontId, new Point2D(windowWidth * 0.5f, windowHeight * 0.5f), TEXT_COLOR, "Hello!!1");
+    }
+}
